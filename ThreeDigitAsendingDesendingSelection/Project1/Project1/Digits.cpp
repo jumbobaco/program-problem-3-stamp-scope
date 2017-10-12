49e8d3b1ecaaf4cd36d157463c9d1504a3e4ffef
jumbobaco <31975654+jumbobaco@users.noreply.github.com>
2017-10-12 11:53:58 -0500

    Comments updated
    10/12/2017

--- ThreeDigitAsendingDesendingSelection/Project1/Project1/Digits.cpp
+++ ThreeDigitAsendingDesendingSelection/Project1/Project1/Digits.cpp
@@ -37,19 +37,18 @@ void main() {
 	int calc_1, var1, var2, var3;
 	cout << "Enter a three digit number" << endl;
 	cin >> calc_1;
-	var1 = calc_1 % 10; // far right
-	var2 = (calc_1 / 10) % 10; // middle
-	var3 = (calc_1 / 100) % 100; // far left
-
+	var1 = calc_1 % 10; // Digit in the ones place
+	var2 = (calc_1 / 10) % 10; // Digit in the tens place
+	var3 = (calc_1 / 100) % 100; // Digit in the hundreds place
 	if ((var1 > var2) && (var2 > var3)) {
-		cout << "ascending" << endl;
+		cout << "ascending" << endl; // prints ascending if the digits go up in value from the hundreds place to the ones place
 	}
 
-	else if ((var3 > var2) && (var2 > var1)) {
+	else if ((var3 > var2) && (var2 > var1)) { // prints descending if the digits go up in value from the hundreds place to the ones place
 		cout << "descending" << endl;
 	}
 	else {
-		cout << "neither" << endl;
+		cout << "neither" << endl; // prints neither if the 3 digit integer doesnt apply to ascending or descending
 	}
 
 	pause(); // pauses to see the displayed text
